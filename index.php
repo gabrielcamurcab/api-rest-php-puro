290eb01df1423e8b3aa05acb2366d126cd084ea1
Gabriel Camurça <gabriel@brtech.dev>
2023-11-28 13:24:21 -0300

    Implements UserModel.php

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode('/', $uri);
 
-print_r($uri);
-exit();
-
 if(isset($uri[1]) && $uri[1] != 'api' || (isset($uri[2]) && $uri[2] != 'v1')) {
     header("HTTP/1.1 404 Not Found");
     exit();
